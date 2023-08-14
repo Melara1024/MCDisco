8fea19f5061dc46a54e2a5e5443972c191c373d2
Melara <105904994+Melara1024@users.noreply.github.com>
2023-08-14 10:34:14 +0900

    Add discord bot lib

--- src/main/java/ga/melara/mcdisco/MCDisco.java
+++ src/main/java/ga/melara/mcdisco/MCDisco.java
@@ -1,11 +1,17 @@
 package ga.melara.mcdisco;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+
+import javax.security.auth.login.LoginException;
 
 @Mod(modid = MCDisco.MODID, name = MCDisco.NAME, version = MCDisco.VERSION)
 public class MCDisco
@@ -16,4 +22,22 @@ public class MCDisco
 
     public static Logger modLogger;
 
+    private static JDA jda = null;
+    private static String BOT_TOKEN = "";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        Config.writeConfig("BOT", "BOT TOKEN", "example token");
+        BOT_TOKEN = Config.getString("BOT", BOT_TOKEN);
+
+        try {
+            jda = JDABuilder.createDefault(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES)
+                    .setRawEventsEnabled(true)
+                    .setActivity(Activity.playing("Crafting"))
+                    .build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+    }
 }
